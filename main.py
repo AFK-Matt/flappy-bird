346f7c169517cbb63900e1ea55c57477585efd78
AFK-Matt <129594@cloud.rangitoto.school.nz>
2021-03-31 14:18:06 +1300

    Added the level and game class. Level class creates an empty canvas, game creates sets up the root and Titlescreen class

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
 """By Matthew Leith"""
 import tkinter as tk
+HEIGHT = 400
+WIDTH = 200
 
-root = tk.Tk()
 
-root.title("Flappy Bird - Matt Leith Edition")
-root.geometry("400x400")
+class Level:
+    def __init__(self, root):
+        self.root = root
+        self.canvas = tk.Canvas(self.root,
+                                height=self.root.winfo_height(),
+                                width=self.root.winfo_width(),
+                                bg="green")
+        self.canvas.pack()
 
 
 class TitleScreen:
-    def __init__(self):
-        self.title_frame = tk.Frame(root)
+    def __init__(self, root):
+        self.root = root
+        self.title_frame = tk.Frame(self.root)
         self.title_label = tk.Label(self.title_frame, text="Welcome to Flappy Bird")
         self.title_label.pack()
         self.play_button = tk.Button(self.title_frame, text="Play", command=self.play)
         self.play_button.pack()
         self.title_frame.pack()
+        self.level = None
 
     def play(self):
         self.title_frame.pack_forget()
+        self.level = Level(self.root)
 
 
-def run_game():
-    new_cress = TitleScreen()
+class Game:
+    def __init__(self):
+        self.root = tk.Tk()
+
+        self.root.title("Flappy Bird - Matt Leith Edition")
+        self.root.geometry("400x400")
+        self.title_screen = TitleScreen(self.root)
+        self.root.mainloop()
 
 
-run_game()
-root.mainloop()
+new_game = Game()
 
 
