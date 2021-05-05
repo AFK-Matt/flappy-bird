e5478bc4b81820b747b6681f50d9f00a9266ea0c
AFK-Matt <129594@cloud.rangitoto.school.nz>
2021-05-05 14:51:49 +1200

    Added the ability to restart by unpacking the canvas and re making the bird. Still has weired bug where canvas does not expand to window

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ HEIGHT = 400
 WIDTH = 400
 BIRD_SIZE = 20  # Width and height of the square bird
 x1 = 30  # Pixels between the right edge of bird and left edge of canvas
-y1 = WIDTH/2  # Pixels from the top of the screen the bottom of the bird spawns
+y1 = WIDTH/4  # Pixels from the top of the screen the bottom of the bird spawns
 MAX_FALL_SPEED = 10
 MS_BETWEEN_TICKS = 10  # Milliseconds between each tick
 GROUND_HEIGHT = 23  # Pixels that the ground is above the bottom of canvas
@@ -19,7 +19,9 @@ class Bird:
         self.box = level.canvas.create_rectangle(x1 - BIRD_SIZE,
                                                  y1 - BIRD_SIZE,
                                                  x1, y1,
-                                                 fill="gold1")
+                                                 fill="gold1",
+                                                 tags="bird")
+        self.level.root.bind('<space>', self.jump)
         self.falling_speed = 0
 
     def jump(self, foo):
@@ -37,22 +39,24 @@ class Bird:
 class Level:
     """Handle the game while the canvas is in use, including ticks."""
 
-    def __init__(self, root):
+    def __init__(self, game):
         """Set up the canvas for the level.
 
         Makes a canvas the size of the window, makes a bird object inside it.
         Sets up the ground graphic, binds the space to the birds jump.
         Runs the tick method.
         """
-        self.root = root
-        canvas_height = self.root.winfo_height()  # Height canvas will be
-        canvas_width = self.root.winfo_width()  # Width canvas will be
+        self.over = False  # I.e. the game is still in progress
+        self.game = game
+        self.root = self.game.root  # Make it shorter to get to
+        canvas_height = self.root.winfo_reqheight()  # Height canvas will be
+        canvas_width = self.root.winfo_reqheight()  # Width canvas will be
         self.canvas = tk.Canvas(self.root,
                                 height=canvas_height,
                                 width=canvas_width,
                                 bg="green")
-        self.canvas.pack()
-        self.bird = Bird(self)  # Make the bird
+        #print(canvas_width, canvas_height)
+        self.bird = None  # Initialise the bird variable
         # Make the ground
         self.ground_box = self.canvas.create_rectangle(0,
                                                        canvas_height
@@ -60,23 +64,39 @@ class Level:
                                                        canvas_width,
                                                        canvas_height,
                                                        fill="grey")
-        self.root.bind('<space>', self.bird.jump)
-        self.tick()
 
     def bird_is_not_touching(self):
         """Return True if the bird is not touching a pipe/the ground."""
         # First see if the bird is above the ground
-        if self.canvas.coords(self.bird.box)[3] >= self.canvas.winfo_width()\
+        if self.canvas.coords(self.bird.box)[3] >= self.canvas.winfo_reqheight()\
                 - GROUND_HEIGHT:
             return False
 
         return True
 
+    def end_level(self):
+        """Unpack the canvas, set level_over to True"""
+        self.over = True
+        self.canvas.pack_forget()
+
+    def start_level(self):
+        """Get the level get to play again and start tick"""
+        self.over = False
+        self.canvas.delete("bird")  # Clear the canvas
+        self.bird = Bird(self)  # Make the bird
+        self.canvas.pack()
+        #print(self.canvas.winfo_height(), self.root.winfo_height())
+        self.tick()
+
     def tick(self):
         """Call methods that need to be called every tick."""
-        print(self.bird_is_not_touching())
+        if self.bird_is_not_touching() is not True:
+            self.end_level()
+            self.game.player_died()
+
         self.bird.weight()
-        self.canvas.after(MS_BETWEEN_TICKS, self.tick)
+        if self.over is False:
+            self.canvas.after(MS_BETWEEN_TICKS, self.tick)
 
 
 class TitleScreen:
@@ -86,19 +106,32 @@ class TitleScreen:
         self.title_label = tk.Label(self.title_frame,
                                     text="Welcome to Flappy Bird")
         self.title_label.pack()
+        self.score_label = tk.Label(self.title_frame,
+                                    text="")
+        self.score_label.pack()
+        self.high_score_label = tk.Label(self.title_frame,
+                                         text="")
+        self.high_score_label.pack()
         self.play_button = tk.Button(self.title_frame,
                                      text="Play",
-                                     command=self.game.start_level)
+                                     command=self.game.play_level)
         self.play_button.pack()
         self.open_title_screen()
 
     def open_title_screen(self):
-        """Pack the title screen's frame"""
+        """Pack the title screen's frame and updates score label"""
+        if self.game.score != -1:  # Update the score if the have one
+            self.score_label["text"] = "Last score: {}".format(
+                self.game.score)
+            self.high_score_label["text"] = "High score: {}".format(
+                self.game.high_score)
         self.title_frame.pack()
 
     def close_title_screen(self):
         """Unpack the title screen's frame"""
+        self.game.root.update()
         self.title_frame.pack_forget()
+        self.game.root.update()
 
 
 class Game:
@@ -107,14 +140,24 @@ class Game:
         self.root = tk.Tk()
         self.root.title("Flappy Bird - Matt Leith Edition")
         self.root.geometry("400x400")
+        self.score = -1  # Not 0 to tell if we have played before
+        self.high_score = 0
+        self.level = Level(self)
         self.title_screen = TitleScreen(self)
-        self.level = None
+
         self.root.mainloop()
 
-    def start_level(self):
+    def play_level(self):
         """Close title screen, make new level object"""
+        self.score = 0
         self.title_screen.close_title_screen()
-        self.level = Level(self.root)
+        self.level.start_level()
+
+    def player_died(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.level.end_level()
+        self.title_screen.open_title_screen()
 
 
 new_game = Game()
